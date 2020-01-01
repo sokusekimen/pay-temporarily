d3fe86c01faf02eedcff3af841a743c8a55ff709
sokusekimen <m.yamagishi9000@gmail.com>
2020-01-02 00:32:33 +0900

    dummyuser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -6,5 +7,6 @@ class ApplicationController < ActionController::Base
       added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
       devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     end
 end
