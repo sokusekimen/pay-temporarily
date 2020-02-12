42cf26893b76619efc91e5a9814e0a5917f899d9
sokusekimen <m.yamagishi9000@gmail.com>
2020-02-13 00:10:19 +0900

    fin 200213

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,14 @@
 module PostsHelper
-  def sum_total_price(id)
+  def sum_total_price id
     group = Group.find_by(id: id)
     price = group.posts.pluck(:total_price)
     return price.sum
   end
 
-  def equally
+  def equally id
+    group = Group.find_by(id: id)
+    group_user_count = group.users.count
+    group_total_price = sum_total_price(id)
+    return group_total_price / group_user_count
   end
 end
