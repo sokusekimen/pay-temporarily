b735f516f7835b62e438586d663dddf2e7b08b17
sokusekimen <m.yamagishi9000@gmail.com>
2020-03-09 22:46:40 +0900

    200309-4

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-users = User.new(username: 'user1', email: 'user1@test.com', password: 'g$hz8vib')
-# users.skip_confirmation!
-users.save!
-
-5.times do |number|
-  user = User.new(username: "user#{number + 2}", email: "user#{number + 2}@test.com", password: 'g$hz8vib')
-  # user.skip_confirmation!
-  user.save!
-end
