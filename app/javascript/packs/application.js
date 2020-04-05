f396dbb59005fd32d4a0b8409f1ce37a6fc2b361
sokusekimen <m.yamagishi9000@gmail.com>
2020-04-06 00:59:05 +0900

    fin200405

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,21 @@
+'use strict';
+
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
 // import '../javascripts/';
 import '../stylesheets/application';
 require.context('../images', true, /\.(png|jpg|jpeg|svg)$/);
+
+window.onload = () => {
+  const getSelectMember = document.getElementById('selectmember');
+  
+  if(getSelectMember){
+    const childElementCount = getSelectMember.childElementCount;
+    if(childElementCount >4){
+      getSelectMember.classList.add('_scrollbar');
+    }
+  }
+}
