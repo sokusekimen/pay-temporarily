988cec8ba6e04e54e0442e5c7e7b74058dc37939
sokusekimen <m.yamagishi9000@gmail.com>
2020-04-16 01:00:48 +0900

    fin200415

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,11 +10,17 @@ require.context('../images', true, /\.(png|jpg|jpeg|svg)$/);
 
 window.onload = () => {
   const getSelectMember = document.getElementById('selectmember');
-  
-  if(getSelectMember){
+  if (getSelectMember) {
     const childElementCount = getSelectMember.childElementCount;
-    if(childElementCount >4){
+    if (childElementCount > 4) {
       getSelectMember.classList.add('scrollbar');
     }
+  }
+
+  const getGroupOwner = document.getElementById('group_owner');
+  if (getGroupOwner) {
+    const getOwnerId = getGroupOwner.value;
+    const getCurrentLabel = document.querySelector(`label[for="group_user_ids_${getOwnerId}"]`);
+    getCurrentLabel.hidden = true;
   }
 }
