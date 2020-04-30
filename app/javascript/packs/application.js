f62694261412c987e43971e1ce669f306be8d62c
sokusekimen <m.yamagishi9000@gmail.com>
2020-04-30 22:39:41 +0900

    不要削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,24 +6,7 @@ require("channels")
 
 // import '../javascripts/';
 import '../stylesheets/application';
-require.context('../images', true, /\.(png|jpg|jpeg|svg)$/);
-
-// window.onload = () => {
-//   const getSelectMember = document.getElementById('selectmember');
-//   if (getSelectMember) {
-//     const childElementCount = getSelectMember.childElementCount;
-//     if (childElementCount > 4) {
-//       getSelectMember.classList.add('scrollbar');
-//     }
-//   }
-
-//   const getGroupOwner = document.getElementById('group_owner');
-//   if (getGroupOwner) {
-//     const getOwnerId = getGroupOwner.value;
-//     const getCurrentLabel = document.querySelector(`label[for="group_user_ids_${getOwnerId}"]`);
-//     getCurrentLabel.hidden = true;
-//   }
-// }
+// require.context('../images', true, /\.(png|jpg|jpeg|svg)$/);
 
 document.addEventListener("DOMContentLoaded", function () {
   const getSelectMember = document.getElementById('selectmember');
